35559cd9e6c58f842c8e92d4d47b7ec151b960b8
the-parthenon <ericinportland@gmail.com>
2022-04-28 11:33:06 -0700

    Added rollbar

--- server/index.js
+++ server/index.js
@@ -4,6 +4,17 @@ const path = require('path');
 
 const app = express();
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar');
+var rollbar = new Rollbar({
+  accessToken: 'b04397f207c544e3b5d72f4ee0bf7ece',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!');
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
